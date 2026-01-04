e94a6961479a63771cc71fba04d3a587a794d7c7
Matteo Gassend <mgassend@gmail.com>
2026-01-04 18:36:31 +0100

    add: render collections

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ fn main() {
     let config_content = fs::read_to_string("./balzac.toml").expect("Config file not found");
     let parsed_config: config::Config = from_str(&config_content).expect("Could not parse config");
     log::info!("Parsed configuration file");
-    let mut render = renderer::HandlebarsRenderer::new(&parsed_config);
+    let mut render: renderer::HandlebarsRenderer<'_> =
+        renderer::HandlebarsRenderer::new(&parsed_config);
     render.init(&parsed_config);
     log::info!("Renderer is initialized");
     if let Err(e) = make_dist_folder(&parsed_config) {
@@ -20,12 +21,12 @@ fn main() {
         std::process::exit(1);
     }
 
-    if let Err(e) = render_static_pages(&parsed_config, render) {
+    if let Err(e) = render_static_pages(&parsed_config, &render) {
         log::error!("Error rendering static pages: {}", e);
         std::process::exit(1);
     }
 
-    if let Err(e) = render_collections(&parsed_config) {
+    if let Err(e) = render_collections(&parsed_config, &render) {
         log::error!("Error rendering collections: {}", e);
         std::process::exit(1);
     }
