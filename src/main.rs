0ac7a7705bc34e7fd75d510f8a4feee0b41330cd
Matteo Gassend <mgassend@gmail.com>
2026-01-10 11:38:16 +0100

    add(wip): parsing hooks and running them

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use shell_words::split;
 use std::fs;
+use std::process::Command;
 use toml::from_str;
 
 use balzac::{
@@ -18,6 +20,34 @@ fn main() {
         renderer::HandlebarsRenderer::new(&parsed_config);
     render.init(&parsed_config);
     log::info!("Renderer is initialized (took {:?})", start.elapsed());
+    if parsed_config.hooks.is_some()
+        && parsed_config
+            .hooks
+            .as_ref()
+            .expect("could not get hooks")
+            .build_before
+            .is_some()
+    {
+        log::info!("Found build_before hook, running");
+        let start = std::time::Instant::now();
+        let hook = &parsed_config
+            .hooks
+            .as_ref()
+            .unwrap()
+            .build_before
+            .as_ref()
+            .unwrap();
+        let parts = split(hook).expect("Invalid command syntax");
+        let mut cmd = Command::new(&parts[0]);
+        for arg in &parts[1..] {
+            cmd.arg(arg);
+        }
+        if let Err(e) = cmd.status() {
+            log::error!("Error running build_before hook: {}", e);
+            std::process::exit(1);
+        }
+        log::info!("build_before hook completed (took {:?})", start.elapsed());
+    }
     let start = std::time::Instant::now();
     if let Err(e) = make_dist_folder(&parsed_config) {
         log::error!("Error creating output directory: {}", e);
@@ -38,6 +68,29 @@ fn main() {
         std::process::exit(1);
     }
     log::info!("Rendered collections (took {:?})", start.elapsed());
+
+    if parsed_config.hooks.is_some() && parsed_config.hooks.as_ref().unwrap().build_after.is_some()
+    {
+        log::info!("Found build_after hook, running");
+        let start = std::time::Instant::now();
+        let hook = &parsed_config
+            .hooks
+            .as_ref()
+            .unwrap()
+            .build_after
+            .as_ref()
+            .unwrap();
+        let parts = split(hook).expect("Invalid command syntax");
+        let mut cmd = Command::new(&parts[0]);
+        for arg in &parts[1..] {
+            cmd.arg(arg);
+        }
+        if let Err(e) = cmd.status() {
+            log::error!("Error running build_after hook: {}", e);
+            std::process::exit(1);
+        }
+        log::info!("build_after hook completed (took {:?})", start.elapsed());
+    }
 
     let start = std::time::Instant::now();
     if let Err(e) = add_assets(&parsed_config) {
